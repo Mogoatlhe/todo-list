cf14c7dc4cbe59f434ffae7b9e93afc850740628
mogoatlhe <iam@ofent.se>
2022-03-22 18:46:47 +0200

    created a template 4 the input tag.

---
+++ src/DOM/html-elements/input.js
@@ -0,0 +1,28 @@
+
+import { Element } from "../element";
+import { Attribute } from "../attribute";
+
+export class Input extends Element{
+
+    #input;
+
+    constructor(className, type, id = undefined){
+        
+        let idAttr;
+        const attrArr = [];
+        const typeAttr = new Attribute("type", type);
+        attrArr.push(typeAttr);
+
+        if(id !== undefined){
+            idAttr = new Attribute("id", id);
+            attrArr.push(idAttr);
+        }
+
+        this.#input = new Element("input", attrArr, className);
+    }
+
+    getInput(){
+        return this.#input.getElement();
+    }
+
+}
